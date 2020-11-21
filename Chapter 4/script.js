eda1bfb5c6b17d5b47771321aac4df9a651ed2fe
Dmitriy Nekrasov <dmitriy.nekrasov@KUF.PGS.DOM>
2020-11-21 21:15:53 +0400

    Add Chapter 4

---
+++ Chapter 4/script.js
@@ -0,0 +1,53 @@
+// Абстрагирование повторов ==================================================================================
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// Абстрагируем этот цикл
+function repeatLog(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(i);
+    }
+}
+
+repeatLog(3);
+
+// Вместо console.log можно использовать любую функцию
+function repeat(n, action) {
+    for (let i = 0; i < n; i++) {
+        action(i);
+    }
+}
+
+
+// Создаем функциональное выражение непосредственно в вызове
+let label = [];
+repeat(5, i => {
+    label.push(`Блок ${i + 1}`);
+});
+console.log(label);
+
+
+// Функции высшего порядка ============================================================================
+// Функция, меняющая свойства другой функции
+function noisy(f) {
+    return(...args) => {
+        console.log('вызов для', args);
+        let result = f(...args);
+        console.log('вызов для ', args, " возвращает", result);
+        return result;
+    }
+}
+
+noisy(Math.min)(3, 2, 1);
+
+// Функция, создающая новый тип последовательности выполнения
+function unless(test, then) {
+    if(!test) then();
+}
+
+repeat(3, n => {
+    unless(n % 2 == 1, () => {
+        console.log(`${n} - четное число`);
+    });
+});
