b7a6d32ceb066247a019cceeae7eb4241fdd7c43
Anna <averbah-ann@mail.ru>
2020-11-23 09:47:03 +0400

    add lesson by vladilen minin

---
+++ Chapter4/minin.js
@@ -0,0 +1,53 @@
+// Методы массивов =========================================================================================
+const people = [
+  {name: 'Владилен', age: 25, budget: 40000},
+  {name: 'Елена', age: 17, budget: 3400},
+  {name: 'Игорь', age: 49, budget: 50000},
+  {name: 'Михаил', age: 15, budget: 1800},
+  {name: 'Василиса', age: 24, budget: 25000},
+  {name: 'Виктория', age: 38, budget: 2300},
+];
+
+// ES5
+/* for (let i = 0; i < people.length; i++) {
+  console.log(people[i]);
+} */
+
+// ES6
+/* for (let person of people) {
+  console.log(person);
+} */
+
+// forEach
+/* people.forEach(person => console.log(person)); */
+
+// Map
+/* const newPeople = people.map(person => person.age * 3);
+console.log(newPeople); */
+
+// Filter
+/* const adults = people.filter(person => person.age >= 18);
+console.log(adults); */
+
+// Reduce
+/* const amount = people.reduce((total, person) => total + person.budget, 0);
+console.log(amount); */
+
+// find
+/* const igor = people.find(person => person.name === 'Игорь');
+console.log(igor); */
+
+// findIndex
+/* const igorIndex = people.findIndex(person => person.name === 'Игорь');
+console.log(igorIndex); */
+
+const amount = people
+    .filter(person => person.budget > 3000)
+    .map(person => {
+      return {
+        info: `${person.name} (${person.age})`,
+        budget: person.budget
+      };
+    })
+    .reduce((total, person) => total + person.budget, 0);
+console.log(amount);
